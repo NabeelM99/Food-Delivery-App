6ccb2593ffd127342b656f4454a31c9c80d86c05
NabeelM99 <nabeelnmm9999@gmail.com>
2025-01-26 14:05:47 +0300

    git commit -m "Initial commit"

--- app/src/main/java/com/example/fooddeliveryapp/MainActivity.kt
+++ app/src/main/java/com/example/fooddeliveryapp/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : ComponentActivity() {
                         }
 
                         // Location Selection Screen
-                        composable("LocationScreen") {
+                        composable("location") {
                             LocationSelectionScreen(navController)
                         }
 
